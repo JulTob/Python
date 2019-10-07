464d59e05347dc38df6055f559d7b92e33c3a980
Julio <JulioToboso@gmail.com>
2019-10-07 20:07:44 +0200

    Update satanic_turtle.py

--- satanic_turtle.py
+++ satanic_turtle.py
@@ -4,10 +4,12 @@ turtle.bgcolor("grey")
 turtle.shape("turtle")
 turtle.color("red","black")
 for i in range(0,5):
-   turtle.forward(100)
+   turtle.forward(161)
    turtle.right(144)
-turtle.right(-144)
+turtle.right(1*72)
 for i in range(0,5):
    turtle.forward(100)
-   
+   turtle.right(-72)
+
+
 turtle.exitonclick()
