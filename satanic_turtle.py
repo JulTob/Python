9d19df8d13c1f6dacb710380015eae6495db40a9
Julio <JulioToboso@gmail.com>
2019-10-07 19:54:09 +0200

    Create satanic_turtle.py

---
+++ satanic_turtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.bgcolor("grey")
+turtle.shape("turtle")
+turtle.color("red","black")
+for i in range(0,5):
+   turtle.forward(100)
+   turtle.right(144)
+turtle.right(-144)
+for i in range(0,5):
+   turtle.forward(100)
+   
+turtle.exitonclick()
