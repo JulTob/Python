5e01ba84c5a4f84b87402ad7e717de9b7d21915d
Julio <JulioToboso@gmail.com>
2019-10-08 13:29:49 +0200

    Update spyder.py

--- spyder.py
+++ spyder.py
@@ -4,14 +4,15 @@ turtle.bgcolor("grey")
 turtle.shape("turtle")
 turtle.color("red","black")
 turtle.pensize(2)
-for iii in range(1,10):
+g = (1 + 5 ** 0.5) / 2
+for iii in range(0,10):
     for ii in range(0,5):
         for i in range(0,5):
-           turtle.forward(161*iii//5)
+           turtle.forward(100*(g**(iii+1)))
            turtle.right(144)
         turtle.right(1*72)
         for i in range(0,5):
-           turtle.forward(100*iii//5)
+           turtle.forward(100*(g**iii))
            turtle.right(-72)
 
 
