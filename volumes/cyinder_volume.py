4a30ae8468d885f1553fdef5b64af0081afd2357
Julio <JulioToboso@gmail.com>
2020-01-27 21:29:05 +0100

    Update and rename cyinder_volume.py to volumes/cyinder_volume.py

--- volumes/cyinder_volume.py
+++ volumes/cyinder_volume.py
@@ -1,7 +1,7 @@
 import math
 radio = float(input("El radio del círculo: "))
-altura = float(input("El radio del círculo: "))
+altura = float(input("Altura del cilindro: "))
 area_circ = math.pi * (radio**2)
 volumen = area_circ * altura
 
-print( round(volumen,3))
+print( round(volumen,6))
