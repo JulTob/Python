28b8bcb204eab936550ee71154263a8ec229f420
Julio <JulioToboso@gmail.com>
2020-02-01 00:46:47 +0100

    Create set_range.py

---
+++ variables/set_range.py
@@ -0,0 +1,11 @@
+# The range of a set of values is the maximum value minus the minimum
+# value. Define a procedure, set_range, which returns the range of three input
+# values.
+
+# Hint: the procedure, biggest which you coded in this unit
+# might help you with this question. You might also like to find a way to
+# code it using some built-in functions.
+
+def set_range(n,m,p):
+    result=max(n,m,p)+max(-n,-m,-p)
+    return result
