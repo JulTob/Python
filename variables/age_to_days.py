590d8dcf13c78678c750afacf7276ec9891b14db
Julio <JulioToboso@gmail.com>
2020-01-31 21:13:43 +0100

    Create age_to_days.py

---
+++ variables/age_to_days.py
@@ -0,0 +1,8 @@
+# Write python code that defines the variable
+# age to be your age in years, and then prints
+# out the number of days you have been alive.
+
+age= 25
+age_mnths= age*12
+age_days= age_mnths*30   #age*365.25
+print(age_days)```
