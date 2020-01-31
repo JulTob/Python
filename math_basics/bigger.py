ee4da9b2ac834ae74b1802e06db1cd21c1de1ea9
Julio <JulioToboso@gmail.com>
2020-02-01 00:35:09 +0100

    Update bigger.py

--- math_basics/bigger.py
+++ math_basics/bigger.py
@@ -10,3 +10,21 @@ def biggest(x,y,z):
 # Well, is actually in the main library. 
 def biggest2(x,y,z):
 	return max(x,y,z)
+
+
+
+def median (x,y,z):
+    a=biggest (x,y,z)
+    b=bigger (x,y)
+    c=bigger (z,y)
+    d=bigger (z,x)
+
+    if x==a:
+        result= c
+    if y==a:
+        result= d
+    if z==a:
+        result= b
+
+    return result
+
