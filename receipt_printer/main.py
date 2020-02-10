80edc8fb8741bae701c34e8edd1d0f1e9334702e
Julio <JulioToboso@gmail.com>
2020-02-10 21:28:04 +0100

    Create main.py

---
+++ receipt_printer/main.py
@@ -0,0 +1,17 @@
+
+
+print("*"*15)
+print('''
+		Coding Business S.A.
+		03300 Orihuela(Alicante)
+		Spain
+''')
+print("="*15)
+print(''' Product   Price'''')
+fof n in List:
+	print(f"	{Product[n]}		{Price[n]}")
+print("="*15)
+print(f"	Total		{total}")
+print("="*15)
+print("Thanks for shopping here!")
+print("*"*15)
