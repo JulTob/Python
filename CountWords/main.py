a2ee36ca0c14eb67bc5c0e32de2ff1e57ce65cee
Julio <JulioToboso@gmail.com>
2020-02-17 13:33:53 +0100

    Create main.py

---
+++ CountWords/main.py
@@ -0,0 +1,19 @@
+import string
+file = open('Alice.txt')
+counts = dict()
+for line in file:
+    line = line.translate(str.maketrans('', '', string.punctuation))
+    line = line.lower()
+    words = line.split()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+# Sort the dictionary by value
+lst = list()
+for key, val in list(counts.items()):
+    lst.append((val, key))
+lst.sort(reverse=True)
+for key, val in lst[:10]:
+    print(key, val)
