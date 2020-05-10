f01d5250fed8803ff637788d5942ea5b9aa37283
Julio <JulioToboso@gmail.com>
2020-05-10 12:27:09 +0200

    Update main.adb

--- name_Generator/main.adb
+++ name_Generator/main.adb
@@ -1,24 +1,39 @@
-import sys, random
+"""Superhero name generator!!!
 
-print("Welcome to Superhero Namer. \n")
+Selects two names to combine them into a superhero name.
+"""
 
-first = (
-"Super", "Spider", "Bat", "Wonder", "Green",
-"Martian", "Magic", "Animal", "Atom"
-)
+import sys
+import random
 
-last = (
-"Man", "Woman", "Kid", "Hero", "Hunter", "Guardian"
-)
+def main():
+    """Choose names at random from 2 tuples of names
+    and print the result"""
+    print("Welcome to Superhero Namer. \n")
 
-while True:
-    firstName = random.choice(first)
-    lastName = random.choice(last)
+    first = (
+    "Super", "Spider", "Bat", "Wonder", "Green",
+    "Martian", "Magic", "Animal", "Atomic", "Fire",
+    "Elastic", "Invisible", "Rock", "Tiguer", "Iron",
+    "Night", "Galactic", "Space", "Electric", "Fantom"
+    )
 
-    print("\n\n")
-    print(f"{firstName}{lastName}", file=sys.stderr)
-    print("\n\n")
+    last = (
+    "Man", "Woman", "Kid", "Hero", "Hunter", "Guardian",
+    "Racoon", "Fist", "Devil", "Storm", "Ghost", "Rider"
+    )
 
-    again = input("Try again? ([n] to quit)\n")
-    if again.lower() == 'n':
-        break
+    while True:
+        firstName = random.choice(first)
+        lastName = random.choice(last)
+
+        print("\n\n")
+        print(f"{firstName}{lastName}", file=sys.stderr)
+        print("\n\n")
+
+        again = input("Try again? ([n] to quit)\n")
+        if again.lower() == 'n':
+            break
+
+if __name__ == "__main__":
+    main()
